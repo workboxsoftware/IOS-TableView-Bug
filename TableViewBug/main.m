bd65d0a35467498a8915d5f444ced9ad35142e52
Bob Glass <bob@Bobs-mbp13.local>
2014-03-20 16:43:54 -0400

    Initial Commit

---
+++ TableViewBug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewBug
+//
+//  Created by Bob Glass on 3/20/14.
+//  Copyright (c) 2014 Workbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TableViewBugTests/TableViewBugTests.m
@@ -0,0 +1,34 @@
+//
+//  TableViewBugTests.m
+//  TableViewBugTests
+//
+//  Created by Bob Glass on 3/20/14.
+//  Copyright (c) 2014 Workbox. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewBugTests : XCTestCase
+
+@end
+
+@implementation TableViewBugTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
